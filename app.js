d143d30664dc1c86da1134f3a7af5b13df30c619
RodrigoOtarola <rodrigo.otarola.gutierrez@ciisa.cl>
2022-09-06 00:09:31 -0300

    Hasta funciones de flecha

--- app.js
+++ app.js
@@ -28,16 +28,45 @@ console.log(`Eliminada : ${eliminado}`);*/
 
 //Ejercicio
 
-const frutas = []
-const fruta = prompt('Que fruta desea comprar?');
+// const frutas = []
+// const fruta = prompt('Que fruta desea comprar?');
+//
+// frutas.push(fruta);
+//
+// while (confirm('¿Desea agregar otro producto?')){
+//     const fruta = prompt('¿Que fruta desea comprar?');
+//     frutas.push(fruta);
+// }
+// console.log('Usted compro: \n');
+// for (let fruta of frutas){
+//     console.log(fruta);
+// }
 
-frutas.push(fruta);
+//Funcion declarativa, siempre podran ser llamadas hoisting
+// function numeroAleatorio(min,max){
+//     return Math.floor(Math.random() * (max-min)) + min;//random para numero aleatorio
+// }
+//
+// console.log(numeroAleatorio(10,20));
 
-while (confirm('¿Desea agregar otro producto?')){
-    const fruta = prompt('¿Que fruta desea comprar?');
-    frutas.push(fruta);
-}
-console.log('Usted compro: \n');
-for (let fruta of frutas){
-    console.log(fruta);
-}
+//Función expresiva, orden logico.
+// const numAzar = function (min, max){
+//     return Math.floor(Math.random() * (max-min)) + min;//random para numero aleatorio
+// }
+//
+// console.log(numAzar(100,200));
+
+//Funciones de flecha, posterior a la flecha mayor sera retornado ejemplo => Math.floor(Math.random() * (max-min)) + min;.
+
+// const azarFlecha =  (min,max) =>
+//     Math.floor(Math.random() * (max-min)) + min;
+//
+// console.log(azarFlecha(20,30));
+
+//Arrow y foreach, con index por defecto traera los numeros iterados
+let frutas = ["Sandia","Melon","Manzana"];
+
+frutas.forEach((fruta,index)=>{
+    console.log(`${index+1} ${fruta}`);
+});
+console.log(frutas);

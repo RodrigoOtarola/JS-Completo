d2755ba5aba5f8169039d7dc3467a88a0efb0652
RodrigoOtarola <rodrigo.otarola.gutierrez@ciisa.cl>
2022-09-06 18:05:10 -0300

    Hasta Getter and Setter

--- objetos.js
+++ objetos.js
@@ -1,23 +1,23 @@
 //Objetos, es una coleccion de propiedades.
 //Properties, nombre que da origen al valor, ejemplo 'nombre':'Rodrigo'
 
-const gato = {
-    'nombre':'Valiente',
-    'duerme':true,
-    'edad':10,
-    'enemigo':['Agua','Perros'],
-    'otros':{
-        'amigos':['cobarde','timidp'],
-        'favoritos':{
-            'comida':{
-                'frio':'Salmon',
-                'caliente':'Pollo',
-            }
-        }
-}
-}
+// const gato = {
+//     'nombre':'Valiente',
+//     'duerme':true,
+//     'edad':10,
+//     'enemigo':['Agua','Perros'],
+//     'otros':{
+//         'amigos':['cobarde','timidp'],
+//         'favoritos':{
+//             'comida':{
+//                 'frio':'Salmon',
+//                 'caliente':'Pollo',
+//             }
+//         }
+// }
+// }
 
-console.log(gato.otros.amigos);
+//console.log(gato.otros.amigos);
 
 // gato.color = 'Azul';
 // gato.edad = 5;
@@ -34,6 +34,59 @@ console.log(gato.otros.amigos);
 
 //Objetos anidados.
 
+// const gato = {
+//     'nombre': 'Valiente',
+//     'duerme': true,
+//     'edad': 10,
+//     'otros':{
+//         'enemigos': ['Agua', 'Perros'],
+//         'amigos':['Cobarde','Timido','Pegajoso']
+//     }
+    /*comer(alimento){
+        console.log(this)//Al ocupar this llama a todo el scope para imprimir, no se puede ocupar en arrow function
+    },
+    //Listar enemigos con forEach
+    listarEnemigos(){
+        this.enemigos.forEach((item)=>console.log(item))
+    }*/
 
+    //Impresion con for in, itera sobre todas las propiedades enumerables de un objeto que codificado en cadenas.
+//}
+/*for (let propiedad in gato) {
+    console.log(gato[propiedad]);
+}*/
 
+//Object.values() (opens new window): devuelve un array con los valores correspondientes a las propiedades enumerables de un objeto.
+/*console.log(Object.values(gato));
+Object.values(gato).forEach((item) => console.log(item))*/
 
+// gato.comer('Pez');
+// gato.listarEnemigos();
+
+//Desestructuracion de objetos, se puede pasar nuevo valor con : ejemplo const {nombre:primerNombre}
+//const {amigos:amigosArray} = gato.otros;
+//console.log(edad);
+
+// const [amigoUno,amigoDos] = amigosArray;
+// console.log(amigoUno,amigoDos)
+
+//Getter y Setter
+
+const gato = {
+    'nombre': 'Valiente',
+    'duerme': true,
+    'edad': 10,
+    'enemigos': ['Agua', 'Perros'],
+    get nombreMayuscula(){
+        return this.nombre.toUpperCase();
+    },
+    set agregarEnemigo(nuevoEnemigo){
+        this.enemigos.push(nuevoEnemigo);
+    }
+};
+//Llamar get
+//console.log(gato.nombreMayuscula);
+
+//Llamar set
+gato.agregarEnemigo = 'Juan';
+console.log(gato.enemigos);

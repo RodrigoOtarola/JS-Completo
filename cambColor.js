5723ac0182bed82c4f4609e0318b9ec484a97c30
RodrigoOtarola <rodrigo.otarola.gutierrez@ciisa.cl>
2022-09-07 17:56:30 -0300

    falta crear el addEventListener

---
+++ cambColor.js
@@ -0,0 +1,7 @@
+const inputColor = document.querySelector("#inputColor");
+const btnVisualizar = document.querySelector("#btnVisualizar");
+const parrafoExa = document.querySelector("#parrafoExa");
+const cardColor = document.querySelector("#pcardColor");
+
+
+

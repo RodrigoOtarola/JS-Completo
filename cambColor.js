972b46a35e3973b3130997e633c1c740fd808324
RodrigoOtarola <rodrigo.otarola.gutierrez@ciisa.cl>
2022-09-07 19:50:39 -0300

    de boton con cambio de color

--- cambColor.js
+++ cambColor.js
@@ -1,7 +1,10 @@
 const inputColor = document.querySelector("#inputColor");
 const btnVisualizar = document.querySelector("#btnVisualizar");
 const parrafoExa = document.querySelector("#parrafoExa");
-const cardColor = document.querySelector("#pcardColor");
+const cardColor = document.querySelector("#cardColor");
 
-
-
+btnVisualizar.addEventListener("click",()=>{
+    console.log(inputColor.value);
+    parrafoExa.textContent = inputColor.value
+    cardColor.style.backgroundColor = inputColor.value;
+})
